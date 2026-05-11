f0ef764e12d1c1947065066b1fd96727f253a34d
krainskigabriel-cell <krainski.gabriel@escola.pr.gov.br>
2026-05-11 20:37:18 -0300

    index.js

--- index.js
+++ index.js
@@ -4,10 +4,8 @@ const rotasItens = require('./routes/itens')
 const app = express();
 const PORT = 3000;
 
-//Habilita o processamento de JSON
 app.use(express.json());
 
-//Define a rota base /itens
 app.use('/itens', rotasItens);
 
 app.listen(PORT, () => {
